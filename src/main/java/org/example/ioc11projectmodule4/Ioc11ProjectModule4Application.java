a834717e43b4943599a1f75b5b1b7d6ea022a39a
Cường Nguyễn Đức <chuoidochoduoi7e@gmail.com>
2026-06-10 14:43:19 +0700

    update

---
+++ src/main/java/org/example/ioc11projectmodule4/Ioc11ProjectModule4Application.java
@@ -0,0 +1,13 @@
+package org.example.ioc11projectmodule4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ioc11ProjectModule4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ioc11ProjectModule4Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ioc11projectmodule4/Ioc11ProjectModule4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ioc11projectmodule4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ioc11ProjectModule4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
